1eefeebce381c76e3ede82d29dcbe8d41119628e
suhendri <hendry45xtc@gmail.com>
2021-11-01 22:01:08 +0700

    update variabel
    menambahkan kata kunci var & final

--- 08-Variable/src/com/tutorial/Main.java
+++ 08-Variable/src/com/tutorial/Main.java
@@ -18,6 +18,21 @@ public class Main {
         b = 7;
 
         System.out.println("nilai b = " + b);
+        
+        /* Kata Kunci var
+        sejak java versi 10, java mendukung pembuatan variabel dengan kata kunci var, sehinggga kita tidak perlu menyebutkan tipe datanya
+        */
+        var name = "Otong";
+        System.out.println(name);
+        
+        /* Kata Kunci final
+        secara default variabel di java dapat diubah- ubah nilainya.
+        jika kita ingin membuat variabel yang nilainya tidak boleh diubah setelah pertama kali dibuat, kita bisa menggunakan kata kunci final.
+        */
+        final String name2 = "Surotong";
+        final var NIK = "111000222000xxxx";
+        
+        System.out.println(name + " " + NIK);
 
 
     }
