62a55e198af650a1caa0c2fdafadb84d74064f4e
suhendri <hendry45xtc@gmail.com>
2021-12-03 19:33:17 +0700

    Update If else if statement
    Menambah contoh baru penggunaan If else if statement

--- 21-If else if statement/src/com/tutorial/Main.java
+++ 21-If else if statement/src/com/tutorial/Main.java
@@ -29,6 +29,21 @@ public class Main {
         // akhir dari if else if statement
 
         System.out.println("ini adalah akhir program");
+        
+        //Contoh penggunaan if else statement
+        
+        int nilai = 80;
+        int absen = 100;
+
+        if (nilai >= 100 && absen >= 100){
+            System.out.println("Nilai Anda A");
+        }else if (nilai >= 80 && absen >= 80){
+            System.out.println("Nilai Anda B");
+        }else if (nilai >= 65 && absen >= 65){
+            System.out.println("Nilai Anda C");
+        }else {
+            System.out.println("Anda Tidak Lulus");
+        }
 
 
     }
