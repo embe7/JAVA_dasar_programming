09dfd4d2b813b576f03f747742135e63d51948cb
suhendri <hendry45xtc@gmail.com>
2021-12-04 21:52:25 +0700

    Update dokumentasi
    menambahkan dokumentasi mengenai do while loop

--- 28-Do While Loop/src/com/tutorial/Main.java
+++ 28-Do While Loop/src/com/tutorial/Main.java
@@ -5,6 +5,13 @@ public class Main {
 
     public static void main (String[] args){
 
+/*
+   - Do while loop adalah perulangan yang mirip dengan while
+   - perbedaannya hanya pada pengecekan kondisi
+   - pengecekan kondisi di while loop dilakukan awal sebelum perulangan dilakukan,
+     sedangkan di do while loop perulangan dilakukan setelah perulangan dilakukan
+   - oleh karena itu didalam do while loop, minimal pasti sekali perulangan dilakukan walaupun kondisi tidak bernilai terue
+*/        
 
 //        do {
 //            aksi
